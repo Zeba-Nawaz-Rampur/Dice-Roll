778f5bedb9118ccae0bed5e61c16507e63f4eaae
ZebaTahseen Rampur <62893751+Zeba-Nawaz-Rampur@users.noreply.github.com>
2026-06-18 20:18:28 +0530

    Add files via upload

---
+++ diceRoll/src/test/java/com/diceroll/diceRoll/DiceRollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diceroll.diceRoll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiceRollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
